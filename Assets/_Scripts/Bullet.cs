1e946581b1326d82973890e6bee0f789ea5c39bb
Siho <sihotastekin123@hotmail.com>
2026-04-09 23:49:16 +0200

    Finished Level 1, Redid Gun Mechanics, Imported Weapon Sprites

--- Assets/_Scripts/Bullet.cs
+++ Assets/_Scripts/Bullet.cs
@@ -1,31 +1,9 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    void OnCollisionEnter(Collision objectWeHit)
+    void OnTriggerEnter(Collider objectWeHit)
     {
-        if (objectWeHit.gameObject.CompareTag("Enemy"))
-        {
-            CreateBulletImpactEffect(objectWeHit);
-            Destroy(gameObject);
-        }
-        if (objectWeHit.gameObject.CompareTag("Terrain"))
-        {
-            CreateBulletImpactEffect(objectWeHit);
-            Destroy(gameObject);
-        }
-    }
-
-    void CreateBulletImpactEffect(Collision objectWeHit)
-    {
-        ContactPoint contact = objectWeHit.contacts[0];
-        GameObject hole = Instantiate(
-            GlobalReferences.Instance.bulletImpactEffectPrefab,
-            contact.point,
-            Quaternion.LookRotation(contact.normal)
-        );
-
-        hole.transform.SetParent(objectWeHit.gameObject.transform);
+        Destroy(gameObject);
     }
 }
