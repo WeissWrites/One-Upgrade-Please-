51c85c06e4df02a0a71634156a6b4969507b658c
Siho <sihotastekin123@hotmail.com>
2026-04-10 23:45:06 +0200

    Finished Adding Guns, Improved Weapon Script, Added Swapping Guns

--- Assets/_Scripts/Weapon.cs
+++ Assets/_Scripts/Weapon.cs
@@ -1,14 +1,18 @@
 using System.Collections;
-using NUnit.Framework;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     public LayerMask shootingLayerMask;
-    public Camera playerCamera;
-    // Shooting
-    public bool isShooting, readyToShoot;
-    bool allowReset = true;
+    public Camera weaponCamera;
+    [Header("Damage Settings")]
+    public int damage;
+    public int headshotDamage;
+    [Header("Shooting Settings")]
+    // Shooting THESE ARE FOR DEBUGGING
+    public bool isShooting;
+    public bool readyToShoot;
+    [Header("Weapon Settings")]
     public float shootingDelay = 2f;
     // Burst
     public int bulletPerBurst = 3;
@@ -59,40 +63,50 @@ public class Weapon : MonoBehaviour
         muzzleEffect.GetComponent<ParticleSystem>().Play();
         readyToShoot = false;
 
-        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Ray ray = weaponCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         Vector3 baseDirection = ray.direction;
 
         // Add Spread
         Vector3 finalDirection = AddSpread(baseDirection);
 
         // Bullet Hit
-        if (Physics.Raycast(playerCamera.transform.position, finalDirection, out RaycastHit hit, 1000f, shootingLayerMask))
+        if (Physics.Raycast(weaponCamera.transform.position, finalDirection, out RaycastHit hit, 1000f, shootingLayerMask))
         {
-            // Handle Damage
-            if (hit.collider.CompareTag("Enemy"))
+            // Check if we hit an Enemy
+            if (hit.collider.CompareTag("Enemy") || hit.collider.CompareTag("Head"))
             {
-                // hit.collider.GetComponent<EnemyHealth>().TakeDamage(damage);
+                // Determine Damage amount
+                int damageToDeal = damage;
+                // Headshot
+                if (hit.collider.CompareTag("Head"))
+                {
+                    damageToDeal = headshotDamage;
+                }
+                // Bodyshot
+                Enemy enemyScript = hit.collider.gameObject.GetComponentInParent<Enemy>();
+
+                if (enemyScript != null)
+                {
+                    enemyScript.TakeDamage(damageToDeal);
+                }
             }
-            // Spawn Impact Effect
+
             CreateImpact(hit);
         }
         // Spawn Bullet Visual
         GameObject tracer = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
         tracer.transform.forward = finalDirection;
         tracer.GetComponent<Rigidbody>().linearVelocity = finalDirection * bulletVelocity; // Set very high velocity
-        StartCoroutine(DestroyBulletAfterTime(tracer, 1f));
-
-        // Handle Reset/Burst logic
-        if (allowReset)
-        {
-            Invoke("ResetShot", shootingDelay);
-            allowReset = true;
-        }
+        StartCoroutine(DestroyBulletAfterTime(tracer, bulletPrefabLifeTime));
         // Shooting in Burst Mode
         if (currentShootingMode == ShootingMode.Burst && burstBulletsLeft > 1)
         {
             burstBulletsLeft--;
             Invoke("FireWeapon", shootingDelay);
+        }
+        else
+        {
+            Invoke("ResetShot", shootingDelay);
         }
     }
     private void CreateImpact(RaycastHit hit)
@@ -108,7 +122,7 @@ public class Weapon : MonoBehaviour
     {
         float x = Random.Range(-spreadIntensity, spreadIntensity);
         float y = Random.Range(-spreadIntensity, spreadIntensity);
-        Vector3 spread = playerCamera.transform.right * x + playerCamera.transform.up * y;
+        Vector3 spread = weaponCamera.transform.right * x + weaponCamera.transform.up * y;
 
         return (baseDir + spread).normalized;
     }
@@ -116,12 +130,14 @@ public class Weapon : MonoBehaviour
     private void ResetShot()
     {
         readyToShoot = true;
-        allowReset = true;
     }
 
     private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
     {
         yield return new WaitForSeconds(delay);
-        Destroy(bullet);
+        if (bullet != null)
+        {
+            Destroy(bullet);
+        }
     }
 }
