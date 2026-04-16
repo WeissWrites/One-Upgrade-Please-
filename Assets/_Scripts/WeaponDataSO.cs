e381d1314d18f30fb5cba003de463ab02d347323
Siho <sihotastekin123@hotmail.com>
2026-04-16 23:49:25 +0200

    Added Debugging button T / Z (Y) for Weapon Rarity, Added Akimbo Weapons, Improved Animations for future, Refactored Weapon.cs

--- Assets/_Scripts/WeaponDataSO.cs
+++ Assets/_Scripts/WeaponDataSO.cs
@@ -1,33 +1,53 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "NewWeaponData", menuName = "ScriptableObjects/WeaponData")]
 public class WeaponDataSO : ScriptableObject
 {
-    [Header("Info")]
+    [System.Serializable]
+    public struct WeaponRarityData
+    {
+        public string rarityName;
+        [Range(0, 3)] public int rarityLevel; // 0=Common, 1=Rare, 2=Epic, 3=Legendary
+
+        [Header("Stats Boosts")]
+        public int damage;
+        public int headshotDamage;
+        public float shootingDelay;
+        public float spreadIntensity;
+        public float snappiness;
+        public float returnSpeed;
+
+        [Header("Attachments Activation")]
+        public bool hasSight;
+        public bool hasLaser;
+        public bool hasGrip;
+    }
+
+    [Header("General Info")]
     public string weaponName;
-    public int weaponRarity;
-
-    [Header("Shooting")]
-    public int damage;
-    public int headshotDamage;
-    public float shootingDelay;
-    public float spreadIntensity;
     public Weapon.ShootingMode shootingMode;
-
-    [Header("Recoil Visuals")]
-    public float kickBackZ;
-    public float kickRotationX;
-    public float snappiness;
-    public float returnSpeed;
-
-    [Header("Ammo")]
     public int magazineSize;
     public int startingReservedAmmo;
     public float reloadTime;
+    public float kickBackZ;
+    public float kickRotationX;
+
+    [Header("Rarity Tiers")]
+    public List<WeaponRarityData> rarityTiers = new List<WeaponRarityData>();
 
     [Header("Visuals & Audio")]
     public AudioClip fireSound;
     public AudioClip reloadSound;
     public AudioClip equipSound;
     public float swapInTime = 0.5f;
+
+    public WeaponRarityData GetRarityData(int level)
+    {
+        foreach (var tier in rarityTiers)
+        {
+            if (tier.rarityLevel == level) return tier;
+        }
+        return rarityTiers[0];
+    }
 }
